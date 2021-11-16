1820949c2f6584e518ee0114fec2f0cb3f62f079
buratsakorn-bew <buratsakorn.boon@mail.kmutt.ac.th>
2021-11-16 23:27:41 +0700

    edit buttonMode

--- Product-Web-HW08/darkmode.js
+++ Product-Web-HW08/darkmode.js
@@ -1,43 +1,50 @@
-let toggleModeBtn = document.getElementById('toggleModeBtn');
-toggleModeBtn.addEventListener('click', (e) => {
+const switchToggle = document.querySelector('input[type="checkbox"]');
+const toggleIcon = document.getElementById("toggle-icon");
+switchToggle.addEventListener("change", (e) => {
   toggleDarkMode();
 });
 
 function toggleDarkMode() {
   if (isDarkModeOn()) {
-    localStorage.setItem('darkmode', false);
+    localStorage.setItem("darkmode", false);
   } else {
-    localStorage.setItem('darkmode', true);
+    localStorage.setItem("darkmode", true);
   }
   updatePage();
 }
 
 function isDarkModeOn() {
-  return localStorage.getItem('darkmode') == 'true';
+  return localStorage.getItem("darkmode") == "true";
 }
 function updatePage() {
   // theme
 
-  let body = document.getElementsByTagName('body')[0];
-  console.log('render');
+  let body = document.getElementsByTagName("body")[0];
+  console.log("render");
   if (isDarkModeOn()) {
-      body.classList.add('bg-dark');
-  }else{
-      body.classList.remove('bg-dark');
+    body.classList.add("bg-dark");
+    switchToggle.checked = true;
+    toggleIcon.children[0].textContent = "DarkMode";
+    toggleIcon.children[1].classList.replace("bxs-sun", "bxs-moon");
+  } else {
+    body.classList.remove("bg-dark");
+    switchToggle.checked = false;
+    toggleIcon.children[0].textContent = "LightMode";
+    toggleIcon.children[1].classList.replace("bxs-moon", "bxs-sun");
   }
 
   //username
 
-  let username = document.getElementById('username');
-  if(getUsername()!=null){
+  let username = document.getElementById("username");
+  if (getUsername() != null) {
     username.innerHTML = getUsername();
   }
 }
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    updatePage();
+window.addEventListener("DOMContentLoaded", (event) => {
+  updatePage();
 });
 
-function getUsername(){
-  return localStorage.getItem('username');
+function getUsername() {
+  return localStorage.getItem("username");
 }
