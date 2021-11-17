1ee6f5ba904caa4f55df3f90a9a64a30ec89f74c
buratsakorn-bew <buratsakorn.boon@mail.kmutt.ac.th>
2021-11-17 18:50:35 +0700

    change photo

--- Product-Web-HW08/products.js
+++ Product-Web-HW08/products.js
@@ -63,7 +63,7 @@ export let products = [{
     stock: "10"
 },
 {
-    img: "image/MagicKeyboard-ThaiK.png",
+    img: "image/MK2A3TH.png",
     id: '4000',
     name: "Magic Keyboard - Thaik",
     price: 3190,
